bd204109f535d9ca797c5630e8694bf99c0293c5
Void <voidpls@protonmail.com>
2018-12-08 04:37:04 -0500

    a

--- modules/recognition.js
+++ modules/recognition.js
@@ -39,7 +39,8 @@ module.exports.run = async msg => {
         if (err) return console.log(err.message)
         if (data.CelebrityFaces.length === 0) return
         data.CelebrityFaces.map(async face => {
-          if (face.Name === 'Ben Shapiro') await msg.react('520825083757985815')
+          if (face.Name === 'Ben Shapiro')
+            await msg.react('520825083757985815').catch(console.error)
         })
       }
     )
